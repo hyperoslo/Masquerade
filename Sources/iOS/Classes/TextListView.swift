7965d109840d2eefb44a62887c6bc3bd2346ca42
Khoa Pham <onmyway133@gmail.com>
2017-02-22 22:53:59 +0100

    Subclass from UIView

--- Sources/iOS/Classes/TextListView.swift
+++ Sources/iOS/Classes/TextListView.swift
@@ -2,8 +2,9 @@ import UIKit
 import Fashion
 import Spots
 import Tailor
+import Cartography
 
-open class TextListView: UITableViewCell, ItemConfigurable {
+open class TextListView: View, ItemConfigurable {
 
   public var item: Item?
 
@@ -17,23 +18,37 @@ open class TextListView: UITableViewCell, ItemConfigurable {
 
   public var preferredViewSize: CGSize = CGSize(width: 0, height: 44)
 
+  public let textLabel = UILabel()
+
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    addSubview(textLabel)
+    setupLayout()
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    fatalError()
+  }
+
   public func configure(_ item: inout Item) {
-    selectionStyle = .none
     let meta: Meta = item.metaInstance()
     styles = meta.styles
 
-    textLabel?.text = item.title
-    detailTextLabel?.text = item.subtitle
+    textLabel.text = item.title
     self.item = item
+  }
+
+  open func setupLayout() {
+    constrain(textLabel) { (textLabel) in
+      textLabel.centerY == textLabel.superview!.centerY
+      textLabel.left == textLabel.superview!.left + 20
+      textLabel.right == textLabel.superview!.right - 20
+    }
   }
 
   open override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
     let result = super.point(inside: point, with: event)
-
-    guard let textLabel = textLabel else {
-      return result
-    }
-
     let insidePoint = convert(point, to: textLabel)
     return result && textLabel.point(inside: insidePoint, with: event)
   }
